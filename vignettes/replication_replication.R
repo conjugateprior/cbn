dfd238d67a61b759ab08e23ca197789a7583ff63
Will Lowe <conjugateprior@gmail.com>
2018-07-02 19:21:44 -0400

    Added a correlation to the replication vignette

--- vignettes/replication_replication.R
+++ vignettes/replication_replication.R
@@ -33,3 +33,35 @@ ggplot(res, aes(x = median, y = 1:nrow(res))) +
   ylab("Careers") +
   xlab("Cosine difference (male - female)")
 
+## ------------------------------------------------------------------------
+its <- cbn_get_items("WEFAT", 2)
+its_vecs <- cbn_item_vectors[its$Word, ]
+res <- wefat_boot(its, its_vecs, x_name = "AndrogynousNames",
+                  a_name = "MaleAttributes", b_name = "FemaleAttributes",
+                  se.calc = "quantile")
+
+## ------------------------------------------------------------------------
+data(cbn_gender_name_stats)
+head(cbn_gender_name_stats)
+
+res <- merge(res, cbn_gender_name_stats, 
+             by.x = "AndrogynousNames", by.y = "name")
+
+## ---- eval = FALSE-------------------------------------------------------
+#  library(gender)
+#  
+#  names <- c("Hugh", "Pugh", "Barney")
+#  gender_name_stats <- gender(names)
+
+## ------------------------------------------------------------------------
+ggplot(res, aes(x = proportion_male, y = diff)) +
+  geom_hline(yintercept = 0, alpha = 0.5, color = "grey") + 
+  geom_point() +
+  geom_text(aes(label = AndrogynousNames), hjust = "left", nudge_x = 0.005) +
+  xlim(0, 1) +
+  xlab("Population proportion male") +
+  ylab("Cosine difference (male - female)")
+
+## ------------------------------------------------------------------------
+cor.test(res$diff,res$proportion_male)
+
